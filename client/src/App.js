391ccfca6a12823284334527fd986e9fbb315216
Geoffrey Gane <geoffreygane@gmail.com>
2020-10-11 14:39:30 +0200

    feat(client): add simple header

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-
+import AppHeader from './components/AppHeader';
 import ResultsTable from './components/ResultsTable';
 
 function App() {
     return (
         <div className="App">
+            <AppHeader />
             <ResultsTable />
         </div>
     );
