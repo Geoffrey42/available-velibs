4aed67f616f534b888a5602734bc97b18541b2d8
Geoffrey Gane <geoffreygane@gmail.com>
2020-10-11 13:53:58 +0200

    feat(client): set states based on server response

--- client/src/components/ResultsTable.js
+++ client/src/components/ResultsTable.js
@@ -1,29 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Header, Table } from 'semantic-ui-react';
 import axios from 'axios';
 
 const url = "http://localhost:4242/api/fetch"
 
 function ResultsTable() {
+    const [records, setRecords] = useState([]);
+    const [hits, setHits] = useState(0);
+    const [total, setTotal] = useState(0);
+
     useEffect(() => {
         const interval = setInterval(async () => {
             try {
                 const response = await axios(url);
 
-                console.log("response: ", response);
+                setRecords(response.data.records);
+                setHits(response.data.nhits);
+                setTotal(response.data.Total);
+
             } catch(err) {
                 console.error("Could not fetch backend server: ", err);
             }
         }, 1000);
         return () => clearInterval(interval);
-    });
+    }, [records, hits, total]);
 
     return (
         <Table>
             <Table.Header>
                 <Table.Row>
                     <Table.HeaderCell>
-                        There are [XXX] available velibs [YYY] meters around Splio's HQ at the moment
+                        There are {total} available velibs [YYY] meters around Splio's HQ at the moment
                     </Table.HeaderCell>
                 </Table.Row>
             </Table.Header>
