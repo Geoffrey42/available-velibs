63c185cb1e5b6a50210083636e1007e9d2130584
Geoffrey Gane <geoffreygane@gmail.com>
2020-10-11 14:05:38 +0200

    feat(client): display distance in table header

--- client/src/components/ResultsTable.js
+++ client/src/components/ResultsTable.js
@@ -8,29 +8,33 @@ function ResultsTable() {
     const [records, setRecords] = useState([]);
     const [hits, setHits] = useState(0);
     const [total, setTotal] = useState(0);
+    const [distance, setDistance] = useState("0");
 
     useEffect(() => {
         const interval = setInterval(async () => {
             try {
                 const response = await axios(url);
 
+                console.log("response.data: ", response.data);
+
                 setRecords(response.data.records);
                 setHits(response.data.nhits);
-                setTotal(response.data.Total);
+                setTotal(response.data.total);
+                setDistance(response.data.Distance);
 
             } catch(err) {
                 console.error("Could not fetch backend server: ", err);
             }
         }, 1000);
         return () => clearInterval(interval);
-    }, [records, hits, total]);
+    }, [records, hits, total, distance]);
 
     return (
         <Table>
             <Table.Header>
                 <Table.Row>
                     <Table.HeaderCell>
-                        There are {total} available velibs [YYY] meters around Splio's HQ at the moment
+                        There are {total} available velibs {distance} meters around Splio's HQ at the moment
                     </Table.HeaderCell>
                 </Table.Row>
             </Table.Header>
